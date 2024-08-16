c760cd8c195dd13ace2885ed6a414c26e7c9f5f8
Mikael Monteiro <mikaelmonteiro562@gmail.com>
2024-08-15 23:18:58 -0300

    Calculadora feita em aula usando csharp

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//int x;
+//Console.WriteLine("Digite o valor de X");
+//x = Convert.ToInt32(Console.ReadLine());
+
+//usando concatenacao
+//Console.WriteLine("O Usuario digitou: " + x + "parabens");
+
+//interpolacao de strings
+//Console.WriteLine($"O usuario digitou: {x} parabens!");
+
+//usando placeholders
+//Console.WriteLine("O usuario digitou: {0} parabens!", x);
+
+int x, y;
+double divisao, multiplicacao, adicao, subtracao;
+
+Console.Write("digite o primeiro valor = ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.Write("digite o segundo valor = ");
+y = Convert.ToInt32(Console.ReadLine());
+
+divisao = (double)x / y;
+multiplicacao = x * y;
+adicao = x + y;
+subtracao = x - y;
+
+Console.WriteLine($"O resultado da divisão de {x} por {y} é: {divisao}"); //interpolaçao
+Console.WriteLine("o valor da adição é de: " + adicao ); //concatenaçao
+Console.WriteLine("O valor da subtração é: {0}", subtracao); //placeholders
+Console.WriteLine("o resultado da multiplicação é: {0}", multiplicacao); //placeholders
+
+
+
